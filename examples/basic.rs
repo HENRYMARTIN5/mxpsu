748ad6c441544a640be157ea4900ff5ea760751e
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2025-06-17 10:32:29 -0500

    fix: Make the channel stay for a sec

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,8 +5,8 @@ fn main() -> Result<(), MxError> {
     let mut psu = MxSeries::connect_serial("/dev/ttyACM0", 9600)?;
 
     psu.turn_on(1)?;
+    std::thread::sleep(std::time::Duration::from_secs(2));
     psu.turn_off(1)?;
 
-    println!("Channel 1 turned on successfully.");
     Ok(())
 }
